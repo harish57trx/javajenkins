530fea5183d3a91270faf6212e6a56549df86576
harish57trx <88072800+harish57trx@users.noreply.github.com>
2021-07-28 15:28:10 +0530

    Update UserApp.java

--- specialisation/GITDEMO2/src/main/java/com/lti/demo2/UserApp.java
+++ specialisation/GITDEMO2/src/main/java/com/lti/demo2/UserApp.java
@@ -3,5 +3,6 @@ package com.lti.demo2;
 public class UserApp {
 public static void main(String[] args) {
 	System.out.println("hello world this is me trying another git push and pull");
+	System.out.println("trying pull request ");
 }
 }
