d31d004677927a4c6e37c86465ef916b460de49f
harish <harishmani@git.com>
2021-07-28 15:57:50 +0530

    commiting in a newbranch

--- specialisation/GITDEMO2/src/main/java/com/lti/demo2/UserApp.java
+++ specialisation/GITDEMO2/src/main/java/com/lti/demo2/UserApp.java
@@ -4,5 +4,12 @@ public class UserApp {
 public static void main(String[] args) {
 	System.out.println("hello world this is me trying another git push and pull");
 	System.out.println("trying pull request ");
+	
+	//this is the steps which i am performing after creating new branch
+	
+	System.out.println("new changes");
+	System.out.println("new beginnings");
+	
 }
+
 }
