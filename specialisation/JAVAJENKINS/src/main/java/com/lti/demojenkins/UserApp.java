b941ecde2a1aa9e9e899aacf12605b31d5f24ee6
harish <harishmani@git.com>
2021-07-29 13:09:10 +0530

    updates jenkin commit

--- specialisation/JAVAJENKINS/src/main/java/com/lti/demojenkins/UserApp.java
+++ specialisation/JAVAJENKINS/src/main/java/com/lti/demojenkins/UserApp.java
@@ -3,5 +3,6 @@ package com.lti.demojenkins;
 public class UserApp {
 public static void main(String[] args) {
 	System.out.println("jenkins automatic building through git");
+	System.out.println("adding some records");
 }
 }
