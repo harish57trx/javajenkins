e788486a351b6228b69b49b1c34b2581d2f1586c
harish <harishmani@git.com>
2021-07-29 13:11:07 +0530

    updated commit and push

--- specialisation/JAVAJENKINS/src/main/java/com/lti/demojenkins/UserApp.java
+++ specialisation/JAVAJENKINS/src/main/java/com/lti/demojenkins/UserApp.java
@@ -4,5 +4,6 @@ public class UserApp {
 public static void main(String[] args) {
 	System.out.println("jenkins automatic building through git");
 	System.out.println("adding some records");
+	System.out.println("enficasidk");
 }
 }
